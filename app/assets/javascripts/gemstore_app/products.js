31b02de87d1f2abfbc9207a1001a39330e8142ec
Phillip Wilt <phillip.wilt@gmail.com>
2014-10-29 13:02:44 -0700

    railsify the gemstore

--- app/assets/javascripts/gemstore_app/products.js
+++ app/assets/javascripts/gemstore_app/products.js
@@ -1,58 +1,53 @@
-(function() {
-  var app = angular.module('store-directives', []);
+gemStoreApp.directive("productGallery", function (){
+  return {
+    restrict: 'E',
+    templateUrl: "templates/products/product-gallery.html",
+    controller: function () {
+      this.current = 0;
+      this.setCurrent = function(imageNumber){
+        this.current = imageNumber || 0;
+      };
+    },
+    controllerAs: 'gallery'
+  };
+});
 
-  app.directive("productGallery", function (){
-    return {
-      restrict: 'E',
-      templateUrl: "js/product-gallery.html",
-      controller: function () {
-        this.current = 0;
-        this.setCurrent = function(imageNumber){
-          this.current = imageNumber || 0;
-        };
-      },
-      controllerAs: 'gallery'
-    };
-  });
+gemStoreApp.directive("productDescription", function() {
+  return {
+    restrict: 'E',
+    templateUrl: "templates/products/product-description.html"
+  };
+});
 
-  app.directive("productDescription", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "js/product-description.html"
-    };
-  });
+gemStoreApp.directive("productReviews", function() {
+  return {
+    restrict: 'E',
+    templateUrl: "templates/products/product-reviews.html"
+  };
+});
 
-  app.directive("productReviews", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "js/product-reviews.html"
-    };
-  });
+gemStoreApp.directive("productSpecs", function () {
+  return {
+    restrict: 'A',
+    templateUrl: "templates/products/product-specs.html"
+  };
+});
 
-  app.directive("productSpecs", function () {
-    return {
-      restrict: 'A',
-      templateUrl: "js/product-specs.html"
-    };
-  });
+gemStoreApp.directive("productTabs", function() {
+  return {
+    restrict: 'E',
+    templateUrl: "templates/products/product-tabs.html",
+    controller: function() {
+      this.tab = 1;
 
-  app.directive("productTabs", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "js/product-tabs.html",
-      controller: function() {
-        this.tab = 1;
+      this.isSet = function(checkTab) {
+        return this.tab === checkTab;
+      };
 
-        this.isSet = function(checkTab) {
-          return this.tab === checkTab;
-        };
-
-        this.setTab = function(setTab) {
-          this.tab = setTab;
-        };
-       },
-      controllerAs: 'tab'
-    };
-  });
-
-})();
+      this.setTab = function(setTab) {
+        this.tab = setTab;
+      };
+     },
+    controllerAs: 'tab'
+  };
+});
