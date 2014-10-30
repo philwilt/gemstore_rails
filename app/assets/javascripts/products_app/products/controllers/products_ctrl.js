40c8808b01006bd533e74e2d491a42dd8f874e8b
Phillip Wilt <phillip.wilt@gmail.com>
2014-10-30 00:14:55 -0700

    index and create for spa products

--- app/assets/javascripts/products_app/products/controllers/products_ctrl.js
+++ app/assets/javascripts/products_app/products/controllers/products_ctrl.js
@@ -1,12 +1,31 @@
 (function (){
   var app = angular.module('ProductsApp');
 
-  app.controller('ProductsCtrl', ['$scope', '$http', function ($scope, $http)
+  app.controller('ProductsCtrl', ['$scope', '$http', function($scope, $http)
   {
-    $scope.error = [];
-    $scope.products = ['Hello!'];
+    $scope.errors = [];
+    $scope.products = [];
+
     $scope.index = function () {
-      $.get('/products')
+      $http.get('/apiv1/products')
+        .success(function(data){
+          $scope.products = data;
+          console.log($scope.products);
+        })
+        .error(function(data,status){
+          $scope.errors.push(data);
+        });
+    };
+
+    $scope.create = function(product) {
+      $http.post('/apiv1/products', { product: product }
+      )
+        .success(function(data){
+          $scope.products.push(data);
+        })
+        .error(function(data,status){
+          $scope.errors.push(data);
+        });
     };
   }])
 })()
