c49b412b0892899d187027942abe2d60ea5baa9a
Phillip Wilt <phillip.wilt@gmail.com>
2014-10-29 20:21:01 -0700

    post endpoint

--- test/integration/create_product_test.rb
+++ test/integration/create_product_test.rb
@@ -18,11 +18,33 @@ class CreateProductTest < ActionDispatch::IntegrationTest
     assert_equal 1000, product[:price].to_i
   end
 
-  test 'does not create a new product with invalid data' do
+  test 'does not create a new product with invalid name' do
     post '/products', { product: {
       name: nil,
       description: 'The best gem',
       price: 1000
+    } }.to_json,
+    { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+
+    assert_equal 422, response.status
+  end
+
+  test 'does not create a new product with invalid description' do
+    post '/products', { product: {
+      name: 'Ruby',
+      description: nil,
+      price: 1000
+    } }.to_json,
+    { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+
+    assert_equal 422, response.status
+  end
+
+  test 'does not create a new product with invalid price' do
+    post '/products', { product: {
+      name: 'Ruby',
+      description: 'The best gem',
+      price: nil
     } }.to_json,
     { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
 
